eeb3eb2d82f610c73986d333b5190088f7b6ea80
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-04-14 18:27:43 +0530

    done

--- models/hsn.py
+++ models/hsn.py
@@ -3,7 +3,7 @@ from extensions import db
 class HSN(db.Model):
     __tablename__ = 'hsn'
     
-    hsn_id = db.Column(db.Integer, primary_key=True,auto_increment=True)
+    hsn_id = db.Column(db.Integer, primary_key=True)
     hsn_code = db.Column(db.String(15), nullable=True, unique=True)
     hsn_description = db.Column(db.String(255), nullable=True)
     gst_rate = db.Column(db.Numeric(10, 2), nullable=True)
