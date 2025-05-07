25b70f38c7c3f4eb7c3b794474569de91d4b3ff8
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-05-07 10:43:38 +0530

    Update order.py

--- routes/order.py
+++ routes/order.py
@@ -554,6 +554,7 @@ def create_order():
         
         # Current date for order_id generation
         current_date = datetime.now()
+        current_year = current_date.year
         
         # Create and add order with explicit order_index
         order = Order(
@@ -574,9 +575,10 @@ def create_order():
             created_at=current_date
         )
         
-        # Manually set the order_id with the expected format
-        date_str = current_date.strftime('%d-%m-%Y')
-        order.order_id = f"{date_str}#{next_order_index}"
+        next_year = current_year + 1
+        next_year = str(next_year)
+        current_year = str(current_year)    
+        order.order_id = f"{current_year}{next_year[2:]}#{next_order_index}"
         
         db.session.add(order)
         db.session.flush()  # Generate order_id
